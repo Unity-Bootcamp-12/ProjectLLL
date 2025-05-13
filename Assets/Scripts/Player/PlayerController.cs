fff79bb0988e918895d7d15287225e6ed96dafbe
aiken516 <jimmy000516@naver.com>
2025-05-13 09:11:17 +0900

    init : 유니티 프로젝트 기본

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private PlayerMovement _playerMovement;
+
+    private void Awake()
+    {
+        _playerMovement = GetComponent<PlayerMovement>();
+    }
+}
