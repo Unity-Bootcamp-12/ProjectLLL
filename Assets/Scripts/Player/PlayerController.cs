1e2b7a0c409ae2fd8155cc3b3514ceae970f3d8b
aiken516 <jimmy000516@naver.com>
2025-05-13 20:01:26 +0900

    [feature] : 추적 이동 로직 구현

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,12 +1,35 @@
+using System;
 using UnityEngine;
 
 public class PlayerController : UnitController
 {
     private PlayerMovement _playerMovement;
+    public bool IsAttackButtonDown { get; private set; }
+
+    private void Start()
+    {
+        PlayerInputManager.Instance.OnLeftClickEvent.AddListener(OnLeftMouseDown);
+        PlayerInputManager.Instance.OnRightClickEvent.AddListener(OnRightMouseDown);
+        PlayerInputManager.Instance.OnAttackButtonEvent.AddListener(OnAttackButtonDown);
+    }
+
+    private void OnLeftMouseDown()
+    {
+    }
+
+    private void OnRightMouseDown()
+    {
+        IsAttackButtonDown = false;
+    }
+
+    private void OnAttackButtonDown()
+    {
+        IsAttackButtonDown = true;
+    }
 
     public override void Dead()
     {
-        IsDead = true
+        IsDead = true;
     }
 
     public override void ReceiveDamage(float damage)
@@ -17,13 +40,5 @@ public class PlayerController : UnitController
     private void Awake()
     {
         _playerMovement = GetComponent<PlayerMovement>();
-    }
-
-    private void Update()
-    {
-        if (Input.GetMouseButtonDown(1))
-        {
-            _playerMovement.OnRightMouseDown();
-        }
     }
 }
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,11 +4,18 @@ using UnityEngine.AI;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private LayerMask _groundMask;
+    [SerializeField] private LayerMask _unitMask;
 
+    private PlayerController _playerController;
     private NavMeshAgent _navMeshAgent;
+
+    const float _moveStoppingDistance = 3.0f;
+
+    private Transform _target;
 
     private void Awake()
     {
+        _playerController = GetComponent<PlayerController>();
         _navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
@@ -18,14 +25,72 @@ public class PlayerMovement : MonoBehaviour
         _navMeshAgent.angularSpeed = 720f;
         _navMeshAgent.stoppingDistance = 0f;
         _navMeshAgent.autoBraking = false; // 시도해보고 미끄러지면 꺼주세요
+
+        PlayerInputManager.Instance.OnRightClickEvent.AddListener(OnRightMouseDown);
+    }
+
+    private void Update()
+    {
+        if (_target != null)
+        {
+            if (Vector3.Distance(transform.position, _target.position) > _moveStoppingDistance)
+            {
+                _navMeshAgent.SetDestination(_target.position);
+            }
+        }
+    }
+
+    public void StopMove()
+    {
+        _navMeshAgent.isStopped = true;
+        _navMeshAgent.ResetPath();
+    }
+
+    public void OnLeftMouseDown()
+    {
+        if (_playerController.IsAttackButtonDown)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out RaycastHit unitHit, 100f, _unitMask))
+            {
+                GameObject hitObject = unitHit.collider.gameObject;
+
+                if (hitObject.TryGetComponent<UnitController>(out var unit))
+                {
+                    Debug.Log($"유닛 감지: {hitObject.name}");
+                    if (unit.TeamType != _playerController.TeamType)
+                    {
+                        _target = hitObject.transform;
+                    }
+                }
+            }
+            else if (Physics.Raycast(ray, out RaycastHit hit, 100f, _groundMask))
+            {
+                _navMeshAgent.SetDestination(hit.point);
+            }
+        }
     }
 
     public void OnRightMouseDown()
     {
+        _target = null;
+
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out RaycastHit hit, 100f, _groundMask))
+
+        if (Physics.Raycast(ray, out RaycastHit unitHit, 100f, _unitMask))
         {
-            _navMeshAgent.SetDestination(hit.point);
+            GameObject hitObject = unitHit.collider.gameObject;
+
+            if (hitObject.TryGetComponent<UnitController>(out var unit))
+            {
+                Debug.Log($"유닛 감지: {hitObject.name}");
+                _target = hitObject.transform;
+            }
+        }
+        else if (Physics.Raycast(ray, out RaycastHit groundHit, 100f, _groundMask))
+        {
+            _navMeshAgent.SetDestination(groundHit.point);
         }
     }
 }
