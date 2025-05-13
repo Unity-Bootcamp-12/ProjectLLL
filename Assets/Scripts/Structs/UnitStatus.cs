5bdcd9dc87eaeca1880e527c8bd08dc6b58dc35a
SBC <seobc789@gmail.com>
2025-05-13 17:30:31 +0900

    [feature] 챔피언(hero) 스탯 시스템 추가
    UnitStatus < HeroScriptableObject < Player, UnitStatusController

---
+++ Assets/Scripts/Structs/UnitStatus.cs
@@ -0,0 +1,12 @@
+using System;
+
+[Serializable]
+public struct UnitStatus
+{
+    // 초기 세팅 값
+    public float MaxHP;
+    public float AttackPower;
+    public float AttackSpeed;
+    public float AttackRange;
+    public float MoveSpeed;
+}
