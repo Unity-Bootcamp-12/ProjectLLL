89dd8e6380366c85f31613c81ec851a0945bb965
ralphlhs <ralphlhs@empas.com>
2025-05-19 13:35:25 +0900

    [Feature] 맵디자인
    맵디자인 초안. 타워에셋, 베이스 Terrain 적용

--- Assets/Scripts/UI/CursorUIController.cs
+++ Assets/Scripts/UI/CursorUIController.cs
@@ -2,90 +2,90 @@ using UnityEngine;
 
 public class CursorUIController : MonoBehaviour
 {
-    private enum CursorType
-    {
-        Default,
-        Attack,
-        Hover
-    }
+    //private enum CursorType
+    //{
+    //    Default,
+    //    Attack,
+    //    Hover
+    //}
 
-    [SerializeField] private Texture2D _cursorDefault;
-    [SerializeField] private Texture2D _cursorAttack;
-    [SerializeField] private Texture2D _cursorHover;
+    //[SerializeField] private Texture2D _cursorDefault;
+    //[SerializeField] private Texture2D _cursorAttack;
+    //[SerializeField] private Texture2D _cursorHover;
 
-    [SerializeField] private LayerMask _unitLayer;
+    //[SerializeField] private LayerMask _unitLayer;
 
-    private CursorType _currentState = CursorType.Default;
-    private bool _isHovering = false;
+    //private CursorType _currentState = CursorType.Default;
+    //private bool _isHovering = false;
 
-    private void Start()
-    {
-        PlayerInputManager.Instance.OnAttackButtonEvent.AddListener(SetAttackCursor);
-        PlayerInputManager.Instance.OnLeftClickEvent.AddListener(SetDefaultCursor);
-        PlayerInputManager.Instance.OnRightClickEvent.AddListener(SetDefaultCursor);
-        SetCursor(CursorType.Default);
-    }
+    //private void Start()
+    //{
+    //    PlayerInputManager.Instance.OnAttackButtonEvent.AddListener(SetAttackCursor);
+    //    PlayerInputManager.Instance.OnLeftClickEvent.AddListener(SetDefaultCursor);
+    //    PlayerInputManager.Instance.OnRightClickEvent.AddListener(SetDefaultCursor);
+    //    SetCursor(CursorType.Default);
+    //}
 
-    private void Update()
-    {
-        CheckHoverTarget();
-    }
+    //private void Update()
+    //{
+    //    CheckHoverTarget();
+    //}
 
-    private void SetDefaultCursor()
-    {
-        if (_currentState != CursorType.Default)
-        {
-            SetCursor(CursorType.Default);
-            _currentState = CursorType.Default;
-        }
-    }
+    //private void SetDefaultCursor()
+    //{
+    //    if (_currentState != CursorType.Default)
+    //    {
+    //        SetCursor(CursorType.Default);
+    //        _currentState = CursorType.Default;
+    //    }
+    //}
 
-    private void SetAttackCursor()
-    {
-        if (_currentState != CursorType.Attack)
-        {
-            SetCursor(CursorType.Attack);
-            _currentState = CursorType.Attack;
-        }
-    }
+    //private void SetAttackCursor()
+    //{
+    //    if (_currentState != CursorType.Attack)
+    //    {
+    //        SetCursor(CursorType.Attack);
+    //        _currentState = CursorType.Attack;
+    //    }
+    //}
 
-    private void SetHoverCursor()
-    {
-        if (_currentState != CursorType.Hover)
-        {
-            SetCursor(CursorType.Hover);
-            _currentState = CursorType.Hover;
-        }
-    }
+    //private void SetHoverCursor()
+    //{
+    //    if (_currentState != CursorType.Hover)
+    //    {
+    //        SetCursor(CursorType.Hover);
+    //        _currentState = CursorType.Hover;
+    //    }
+    //}
 
-    public void SetCursor(CursorType type)
-    {
-        switch (type)
-        {
-            case CursorType.Attack:
-                Cursor.SetCursor(_cursorAttack, Vector2.zero, CursorMode.Auto);
-                break;
-            case CursorType.Hover:
-                Cursor.SetCursor(_cursorHover, Vector2.zero, CursorMode.Auto);
-                break;
-            default:
-                Cursor.SetCursor(_cursorDefault, Vector2.zero, CursorMode.Auto);
-                break;
-        }
-    }
+    //public void SetCursor(CursorType type)
+    //{
+    //    switch (type)
+    //    {
+    //        case CursorType.Attack:
+    //            Cursor.SetCursor(_cursorAttack, Vector2.zero, CursorMode.Auto);
+    //            break;
+    //        case CursorType.Hover:
+    //            Cursor.SetCursor(_cursorHover, Vector2.zero, CursorMode.Auto);
+    //            break;
+    //        default:
+    //            Cursor.SetCursor(_cursorDefault, Vector2.zero, CursorMode.Auto);
+    //            break;
+    //    }
+    //}
 
-    private void CheckHoverTarget()
-    {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        _isHovering = Physics.Raycast(ray, out RaycastHit unitHit, 100.0f, _unitLayer) && unitHit.transform.tag != "Player"; // 같은 팀은 호버상태 안키도록 수정 필요
+    //private void CheckHoverTarget()
+    //{
+    //    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+    //    _isHovering = Physics.Raycast(ray, out RaycastHit unitHit, 100.0f, _unitLayer) && unitHit.transform.tag != "Player"; // 같은 팀은 호버상태 안키도록 수정 필요
 
-        if (_isHovering && _currentState != CursorType.Attack)
-        {
-            SetHoverCursor();
-        }
-        else if (!_isHovering && _currentState != CursorType.Attack)
-        {
-            SetDefaultCursor();
-        }
-    }
+    //    if (_isHovering && _currentState != CursorType.Attack)
+    //    {
+    //        SetHoverCursor();
+    //    }
+    //    else if (!_isHovering && _currentState != CursorType.Attack)
+    //    {
+    //        SetDefaultCursor();
+    //    }
+    //}
 }
