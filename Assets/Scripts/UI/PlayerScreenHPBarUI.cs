93db215a03b1c596ad850b50f58f33add9bc0948
SBC <seobc789@gmail.com>
2025-05-25 00:18:10 +0900

    [Feature] 상단 UI 개선
    - HP 수치 표시 추가

--- Assets/Scripts/UI/PlayerScreenHPBarUI.cs
+++ Assets/Scripts/UI/PlayerScreenHPBarUI.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -5,15 +6,21 @@ public class PlayerScreenHPBarUI : MonoBehaviour
 {
     [SerializeField] private Slider _redTeamHPBar;
     [SerializeField] private Slider _blueTeamHPBar;
+    [SerializeField] private TMP_Text _redTeamHPText;
+    [SerializeField] private TMP_Text _blueTeamHPText;
 
     public void UpdateRedTeamHPBar(float maxHP, float currentHP)
     {
         UpdateUI(_redTeamHPBar, maxHP, currentHP);
+
+        UpdateHPText(_redTeamHPText, maxHP, currentHP);
     }
 
     public void UpdateBlueTeamHPBar(float maxHP, float currentHP)
     {
         UpdateUI(_blueTeamHPBar, maxHP, currentHP);
+
+        UpdateHPText(_blueTeamHPText, maxHP, currentHP);
     }
 
     private void UpdateUI(Slider targetBar, float maxHP, float currentHP)
@@ -21,6 +28,18 @@ public class PlayerScreenHPBarUI : MonoBehaviour
         if (currentHP >= 0 && maxHP > 0)
         { 
             targetBar.value = currentHP / maxHP;
+        }
+    }
+
+    private void UpdateHPText(TMP_Text targetText, float maxHP, float currentHP)
+    {
+        if (currentHP >= 0 && maxHP > 0)
+        {
+            targetText.text = $"{currentHP}/{maxHP}";
+        }
+        else
+        {
+            targetText.text = "0/0";
         }
     }
 }
