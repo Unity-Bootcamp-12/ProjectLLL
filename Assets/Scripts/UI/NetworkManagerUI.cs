86e5365b17e7a46beacff9504dbf9fc23035c670
aiken516 <jimmy000516@naver.com>
2025-05-28 12:36:15 +0900

    [feature] : 연결 UI 기초 구현

--- Assets/Scripts/UI/NetworkManagerUI.cs
+++ Assets/Scripts/UI/NetworkManagerUI.cs
@@ -1,4 +1,6 @@
+using TMPro;
 using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,17 +8,22 @@ public class NetworkManagerUI : MonoBehaviour
 {
     [SerializeField] Button _hostButton;
     [SerializeField] Button _clientButton;
+    [SerializeField] TMP_InputField _ipInputField;
 
     private void Start()
     {
         _hostButton.onClick.AddListener(() =>
         {
+            NetworkManager.Singleton.GetComponent<UnityTransport>().ConnectionData.Address = "0.0.0.0";
             NetworkManager.Singleton.StartHost();
             gameObject.SetActive(false);
         });
 
         _clientButton.onClick.AddListener(() =>
         {
+            string ip = _ipInputField.text;
+            NetworkManager.Singleton.GetComponent<UnityTransport>().ConnectionData.Address = ip;
+
             NetworkManager.Singleton.StartClient();
             gameObject.SetActive(false);
         });
